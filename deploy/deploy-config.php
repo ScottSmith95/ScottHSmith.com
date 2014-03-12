4e9ff34c9e1e4ea76df3583ab29556973dd850b6
Scott Smith <mail@ScottHSmith.com>
2014-03-12 08:45:27 -0500

    Updated deploy script

---
+++ deploy/deploy-config.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The repos that we want to deploy.
+ *
+ * Each repos will be an entry in the array in the following way:
+ * 'repo name' => array( // Required. This is the repo name
+ * 		'path' 	 => '/path/to/local/repo/' // Required. The local path to your code.
+ * 		'branch' => 'the_desired_deploy_branch', // Required. Deployment branch.
+ *		'remote' => 'git_remote_repo', // Optional. Defaults to 'origin'
+ * 		'post_deploy' => 'callback' // Optional callback function for whatever.
+ * )
+ *
+ * You can put as many of these together as you want, each one is simply 
+ * another entry in the $repos array. To set up a deploy create a deploy key
+ * for your repo on github or bitbucket. You can generate multiple deploy keys
+ * for multiple repos.
+ * @see https://confluence.atlassian.com/pages/viewpage.action?pageId=271943168
+ *
+ * Note that deploy keys are only necessary if the repo is private. If it is a
+ * public repo, then you do not need a key to get read only access to the repo
+ * which is really what we are after for deployment.
+ *
+ * Once you have done an initial git pull in the desired code location, you can
+ * run 'pwd' to get the full directory of your git repo. Once done, enter that
+ * full path in the 'path' option for that repo. The optional callback will allow
+ * you to ping something else as well such as hitting a DB update script or any
+ * other configuration you may need to do for the newly deployed code.
+ */
+$repos = array(
+	'My-Personal-Website' => array(
+		'branch' => 'master',
+		'remote' => 'origin',
+		'path' => '/var/www/scotthsmith.com/root'
+	)
+);
+
+/**
+ * Sets the deploy log direcotry
+ */
+define( 'DEPLOY_LOG_DIR', dirname( __FILE__ ) );
+
+/* Do not edit below this line */
+require_once 'inc/class.deploy.php';
---
+++ deploy/inc/class.deploy.php
@@ -0,0 +1,199 @@
+<?php
+// Causes the script to die if we are not using an actual enpoint to access it.
+if( ! defined( 'ACTIVE_DEPLOY_ENDPOINT' ) || true !== ACTIVE_DEPLOY_ENDPOINT )
+	die( '<h1>No Access</h1><p>An enpoint needs to be defined to use this file.</p>' );
+/**
+ * The main Deploy class. This is set up for GIT repos.
+ *
+ * To create an end point, extend this abstract class and in the new class' constructor
+ * parse whatever post payload and pass it to parent::construct(). The data passed should
+ * be an array that is in the following order (note this is right in line with how the
+ * config arrays are put together).
+ * 'repo name' => array(
+ * 		'name'   => 'repo name', // Required
+ * 		'path' 	 => '/path/to/local/repo/' // Required
+ * 		'branch' => 'the_desired_deploy_branch', // Required
+ * 		'commit' => 'the SHA of the commit', // Optional. The SHA is only used in logging.
+ *		'remote' => 'git_remote_repo', // Optional. Defaults to 'origin'
+ * 		'post_deploy' => 'callback' // Optional callback function for whatever.
+ * )
+ *
+ * The parent constructor will take care of the rest of the setup and deploy.
+ *
+ * @todo move the logging functions to a separate class to separate the functionality.
+ */
+
+abstract class Deploy {
+	/**
+	 * Registered deploy repos
+	 */
+	protected static $repos = array();
+
+	/**
+	 * The name of the file that will be used for logging deployments. Set 
+	 * to false to disable logging.
+	 */
+	private static $_log_name = 'deployments.log';
+
+	/**
+	 * The path to where we wish to store our log file.
+	 */
+	private static $_log_path = DEPLOY_LOG_DIR;
+
+	/**
+	 * The timestamp format used for logging.
+	 * 
+	 * @link    http://www.php.net/manual/en/function.date.php
+	 */
+	private static $_date_format = 'Y-m-d H:i:sP';
+	
+	/**
+	 * Registers available repos for deployement
+	 *
+	 * @param array $repo The repo information and the path information for deployment
+	 * @return bool True on success, false on failure.
+	 */
+	public static function register_repo( $name, $repo ) {
+		if ( ! is_string( $name ) )
+			return false;
+
+		if ( ! is_array( $repo ) )
+			return false;
+		
+		$required_keys = array( 'path', 'branch' );
+		foreach ( $required_keys as $key ) {
+			if ( ! array_key_exists( $key, $repo ) )
+				return false;
+		}
+
+		$defaults = array(
+			'remote'      => 'origin',
+			'post_deploy' => '',
+			'commit'      => '',
+		);
+		$repo = array_merge( $defaults, $repo );
+
+		self::$repos[ $name ] = $repo;
+	}
+
+	/**
+	 * Allows alternate log locations and date formats
+	 *
+	 * @return void.
+	 */
+	public static function set( $var, $value ) {
+		if ( ( 'log_name' === $var || 'date_format' === $var ) && is_string( $value ) )
+			self::${'_'.$var} = $value;
+	}
+
+	/**
+	 * Whether or not we are ready to deploy
+	 */
+	private $_deploy_ready;
+
+	/**
+	 * The name of the repo we are attempting deployment for.
+	 */
+	private $_name;
+
+	/**
+	 * The name of the branch to pull from.
+	 */
+	private $_branch;
+
+	/**
+	 * The name of the remote to pull from.
+	 */
+	private $_remote;
+
+	/**
+	 * The path to where your website and git repository are located, can be 
+	 * a relative or absolute path
+	 */
+	private $_path;
+
+	/**
+	 * A callback function to call after the deploy has finished.
+	 */
+	private $_post_deploy;
+
+	/**
+	 * The commit that we are attempting to deploy
+	 */
+	private $_commit;
+
+	/**
+	 * Sets up the repo information.
+	 * 
+	 * @param 	array 	$repo 	The repository info. See class block for docs.
+	 */
+	protected function __construct( $name, $repo ) {
+		$this->_path = realpath( $repo['path'] ) . DIRECTORY_SEPARATOR;
+
+		$this->_name = $name;
+
+		$available_options = array( 'branch', 'remote', 'commit', 'post_deploy' );
+
+		foreach ( $repo as $option => $value ){
+			if ( in_array( $option, $available_options ) ){
+				$this->{'_'.$option} = $value;
+			}
+		}
+
+		$this->execute();
+	}
+
+	/**
+	 * Writes a message to the log file.
+	 * 
+	 * @param 	string 	$message 	The message to write
+	 * @param 	string 	$type 		The type of log message (e.g. INFO, DEBUG, ERROR, etc.)
+	 */
+	protected function log( $message, $type = 'INFO' ) {
+		if ( self::$_log_name ) {
+			// Set the name of the log file
+			$filename = self::$_log_path . '/' . rtrim( self::$_log_name, '/' );
+
+			if ( ! file_exists( $filename ) ) {
+				// Create the log file
+				file_put_contents( $filename, '' );
+
+				// Allow anyone to write to log files
+				chmod( $filename, 0666 );
+			}
+
+			// Write the message into the log file
+			// Format: time --- type: message
+			file_put_contents( $filename, date( self::$_date_format ) . ' --- ' . $type . ': ' . $message . PHP_EOL, FILE_APPEND );
+		}
+	}
+
+	/**
+	* Executes the necessary commands to deploy the code.
+	*/
+	private function execute() {
+		try {
+			// Make sure we're in the right directory
+			chdir( $this->_path);
+
+			// Discard any changes to tracked files since our last deploy
+			exec( 'git reset --hard HEAD', $output );
+
+			// Update the local repository
+			exec( 'git pull ' . $this->_remote . ' ' . $this->_branch, $output );
+
+			// Secure the .git directory
+			echo exec( 'chmod -R og-rx .git' );
+
+			if ( is_callable( $this->_post_deploy ) )
+				call_user_func( $this->_post_deploy );
+
+			$this->log( '[SHA: ' . $this->_commit . '] Deployment of ' . $this->_name . ' from branch ' . $this->_branch . ' successful' );
+		} catch ( Exception $e ) {
+			$this->log( $e, 'ERROR' );
+		}
+	}
+}
+// Registers all of our repos with the Deploy class
+foreach ( $repos as $name => $repo )
+	Deploy::register_repo( $name, $repo );
