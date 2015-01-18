dcd03edec4c49250de8ebf19baf74c9920d1343c
Scott Smith <mail@ScottHSmith.com>
2015-01-18 13:59:50 -0800

    Remove duplicate functionality from Gruntfile in favor of using Gulp

--- gruntfile.js
+++ gruntfile.js
@@ -4,70 +4,6 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		
-		csscomb: {
-			comb_main: {
-				build: [{
-					expand: true,
-					src: ['css/*.scss']
-				}]
-			},
-			comb_tea: {
-				build: [{
-					expand: true,
-					src: ['tea/*.scss']
-				}]
-			},
-		},
-		
-		sass: {
-			options: {
-				outputStyle: 'nested',
-				sourceMap: true
-			},
-			build_main: {
-				files: [{
-					expand: true,
-					cwd: 'css/',
-					src: ['*.scss'],
-					dest: 'css/build/',
-					ext: '.css'
-				}]
-			},
-			build_tea: {
-				files: [{
-					expand: true,
-					src: ['tea/*.scss'],
-					ext: '.css'
-				}]
-			}
-		},
-        
-		autoprefixer: {
-            options: {
-				browsers: ['last 2 versions', '> 1%', 'ie 9', 'ie 8', 'Firefox ESR', 'Opera 12.1'],
-				map: true
-			},
-			prefix_main: {
-				expand: true,
-				src: ['css/build/*.css']
-			},
-			prefix_tea: {
-				expand: true,
-				src: ['tea/*.css']
-			}
-		},
-		
-		cssmin: {
-			minify_main: {
-				expand: true,
-				src: ['css/build/*.css']
-			},
-			minify_tea: {
-				expand: true,
-				src: ['tea/*.css']
-			}
-		},
-		
 		modernizr: {
 			makefile: {
 				"devFile": "remote", // Skip check for dev file
@@ -96,22 +32,6 @@ module.exports = function(grunt) {
 			}
 		},
 		
-		uglify: {
-			options: {
-				sourceMap: true
-			},
-			build_main: {
-				files: {
-					'scripts/build/main.js': ['scripts/modernizr.js', 'bower_components/fastclick/lib/fastclick.js', 'scripts/main.js']
-				}
-			},
-			build_portfolio: {
-				files: {
-					'scripts/build/portfolio.js': ['bower_components/imagesloaded/imagesloaded.pkgd.min.js', 'bower_components/masonry/dist/masonry.pkgd.min.js', 'scripts/portfolio.js']
-				}
-			}
-		},
-		
 		imageoptim: {
 			optimize: {
 				src: ['images/**/*.jpg', 'images/**/*.png'],
@@ -131,14 +51,6 @@ module.exports = function(grunt) {
 		},
 
         watch: {
-			styles: {
-				files: ['css/*.scss'],
-				tasks: ['csscomb:comb_main', 'sass:build_main', 'autoprefixer:prefix_main', 'cssmin:minify_main']
-			},
-			scripts: {
-				files: ['scripts/*.js'],
-				tasks: ['uglify']
-			},
 			images: {
 				files: ['images/**/*.jpg', 'images/**/*.png'],
 				tasks: ['newer:imageoptim']
@@ -151,12 +63,7 @@ module.exports = function(grunt) {
     });
     
     // Plugin List
-    grunt.loadNpmTasks('grunt-csscomb');
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks("grunt-modernizr");
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-newer');
 	grunt.loadNpmTasks('grunt-imageoptim');
 	grunt.loadNpmTasks('grunt-sitemap');
@@ -165,12 +72,7 @@ module.exports = function(grunt) {
 	// Workflows
 	// $ grunt: Concencates, prefixes, minifies JS and CSS files. The works.
 	grunt.registerTask('default', [
-		'csscomb',
-		'sass',
-		'autoprefixer',
-		'cssmin',
 		'modernizr',
-		'uglify',
 		'newer:imageoptim',
 		'sitemap',
 		'watch'
