839ffcbe8a8847d0ab43ff17963ad313714e810b
Scott Smith <mail@ScottHSmith.com>
2015-04-23 16:46:20 -0700

    Scrapped Sass, now using only PostCSS plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 var gulp       = require('gulp'),
 	merge      = require('merge-stream'),
 	kit        = require('gulp-kit'),
-	csscomb    = require('gulp-csscomb'),
-	sass       = require('gulp-sass'),
 	postcss    = require('gulp-postcss'),
 	concat     = require('gulp-concat'),
 	uglify     = require('gulp-uglify'),
@@ -11,8 +9,8 @@ var gulp       = require('gulp'),
 
 var paths = {
 	html:             ['**/*.kit', '!kit-includes/**', '!bower_components/**/*', '!node_modules/**/*'],
-	styles:           'css/*.scss',
-	teaStyles:        'tea/*.scss',
+	styles:           'css/*.css',
+	teaStyles:        'tea/*.css',
 	mainScripts:      ['scripts/modernizr.js', 'bower_components/fastclick/lib/fastclick.js', 'scripts/main.js'],
 	portfolioScripts: ['bower_components/imagesloaded/imagesloaded.pkgd.min.js', 'bower_components/masonry/dist/masonry.pkgd.min.js', 'scripts/portfolio.js'],
 	sitemap:          ['**/*.html', '!error/*.html', '!bower_components/**/*', '!node_modules/**/*']
@@ -26,22 +24,21 @@ gulp.task('html', function(){
 
 gulp.task('styles', function() {
 	var processors = [
-		require('autoprefixer-core')('last 2 versions', '> 1%', 'ie 9', 'ie 8', 'Firefox ESR'),
+		require('postcss-import'),
+		require('postcss-nested'),
+		require('postcss-simple-vars'),
 		require('css-mqpacker'),
+		require('autoprefixer-core')('last 2 versions', '> 1%', 'ie 9', 'ie 8', 'Firefox ESR'),
 		require('csswring')
     ];
 	var mainStyles = gulp.src(paths.styles)
 		.pipe(sourcemaps.init())
-			.pipe(csscomb())
-			.pipe(sass())
 			.pipe(postcss(processors))
 		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest('css/build/'));
 		
 	var teaStyles =  gulp.src(paths.teaStyles)
 		.pipe(sourcemaps.init())
-			.pipe(csscomb())
-			.pipe(sass())
 			.pipe(postcss(processors))
 		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest('tea/'));
