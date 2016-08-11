b8082d18b3ade9630433caad7aed89db98ff708f
Scott Smith <mail@scotthsmith.com>
2016-08-11 01:21:40 -0400

    Updated image replacement.

--- scripts/home.js
+++ scripts/home.js
@@ -2,7 +2,7 @@ boomsvgloader.load('/images/Social%20Icons/home-sprite.svg');
 
 function changeSource() {
     var image = document.querySelectorAll('.header-image')[0];
-    var source = image.src = image.src.replace('ScottPic_600.jpg', 'SexyScott.jpg');
+    var source = image.src = image.src.replace('FullPic.jpg', 'SexyScott.jpg');
 }
 
 if (window.location.host == 'scottsmith.sexy') {
