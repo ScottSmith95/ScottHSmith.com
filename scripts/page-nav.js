71d63910059c5a3eeaaa8c86032b20eaaceabe32
Scott Smith <mail@scotthsmith.com>
2016-08-19 17:23:15 -0400

    Updated page-nav dropdowns.

--- scripts/page-nav.js
+++ scripts/page-nav.js
@@ -2,6 +2,7 @@ var navTrigger = document.querySelector('.nav h1');
 var nav        = document.querySelector('.page-nav');
 
 function toggleClass() {
+	navTrigger.classList.toggle('active');
     nav.classList.toggle('active');
 }
 
