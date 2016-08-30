817591bb5842d617252166b796210c6052dea921
Scott Smith <mail@scotthsmith.com>
2016-08-29 21:37:55 -0400

    Improved page nav accessibility thanks to http://heydonworks.com/practical_aria_examples/.

--- scripts/page-nav.js
+++ scripts/page-nav.js
@@ -1,9 +1,20 @@
 var navTrigger = document.querySelector('.nav h1');
 var nav        = document.querySelector('.page-nav');
 
-function toggleClass() {
+function toggleMenu() {
 	navTrigger.classList.toggle('active');
     nav.classList.toggle('active');
+    
+    if (nav.getAttribute('aria-hidden') == 'true') {
+	    nav.setAttribute('aria-hidden', 'false');
+    }
+    else if (nav.getAttribute('aria-hidden') == 'false') {
+	    nav.setAttribute('aria-hidden', 'true');
+    }
 }
 
-navTrigger.addEventListener('click', toggleClass)
+navTrigger.setAttribute('aria-haspopup', 'true');
+nav.setAttribute('aria-hidden', 'true');
+nav.setAttribute('aria-label', 'submenu');
+
+navTrigger.addEventListener('click', toggleMenu);
