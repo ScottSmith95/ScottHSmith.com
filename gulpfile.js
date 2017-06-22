00aeda7f7d72956b08c4902a6f39d5210a416270
Scott Smith <mail@ScottHSmith.com>
2017-06-22 23:25:05 +0000

    Named all functions. Improved code quality and readability.

--- gulpfile.js
+++ gulpfile.js
@@ -10,14 +10,33 @@ var gulp       = require('gulp'),
 	sourcemaps = require('gulp-sourcemaps');
 
 var paths = {
-	html:              ['**/*.kit', '!kit-includes/**', '!node_modules/**/*'],
-	styles:            ['styles/**/*.css', '!styles/build/**', '!styles/variables.css', '!styles/partials/_*'],
-	teaStyles:         ['tea/*.css'],
-	builtStyles:       ['styles/build/home.css'],
-	sprites:           ['images/Social Icons/*.svg', '!images/Social Icons/home-sprite.svg'],
-	sitemap:           ['**/*.html', '!error/*.html', '!node_modules/**/*'],
-	scripts:           ['scripts/*.js', '!scripts/build/**',
-						'!scripts/main.js', '!scripts/home.js', '!scripts/social-icons.js'],
+	html:              {
+		src: ['**/*.kit', '!kit-includes/**', '!node_modules/**/*'],
+		dest: './'
+	},
+	styles:            {
+		src: ['styles/**/*.css', '!styles/build/**', '!styles/variables.css', '!styles/partials/_*'],
+		dest: 'styles/build/'
+	},
+	teaStyles:         {
+		src: 'tea/*.css',
+		dest: 'tea/build/'
+	},
+	builtStyles:       {
+		src: 'styles/build/home.css'
+	},
+	sprites:           {
+		src: ['images/Social Icons/*.svg', '!images/Social Icons/home-sprite.svg'],
+		dest: 'images/Social Icons'
+	},
+	sitemap:           {
+		src: ['**/*.html', '!error/*.html', '!node_modules/**/*'],
+		dest: './'
+	},
+	scripts:           {
+		src: ['scripts/*.js', '!scripts/build/**', '!scripts/main.js', '!scripts/home.js', '!scripts/social-icons.js'],
+		dest: 'scripts/build/'
+	},
 	mainScript:        ['scripts/vendor/modernizr.js'],
 	homeScript:        ['node_modules/boomsvgloader/dist/js/boomsvgloader.js', 'scripts/home.js'],
 	socialIconsScript: ['node_modules/flickity/dist/flickity.pkgd.js', 'scripts/social-icons.js']
@@ -32,38 +51,39 @@ var processors = [
 	require('autoprefixer')
 ];
 
-gulp.task(function html() {
-	return gulp.src(paths.html)
+// Tasks
+function html() {
+	return gulp.src(paths.html.src)
 		.pipe(kit())
-		.pipe(gulp.dest('./'));
-});
+		.pipe(gulp.dest(paths.html.dest));
+}
 
-gulp.task(function styles() {
+function styles() {
 	processors.push(require('cssnano')({autoprefixer: false, reduceIdents: false}));
 	
-	return gulp.src(paths.styles)
+	return gulp.src(paths.styles.src)
 		.pipe(sourcemaps.init())
 			.pipe(postcss(processors))
 		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest('styles/build/'));
-});
+		.pipe(gulp.dest(paths.styles.dest));
+}
 
-gulp.task(function teaStyles() {
-	return gulp.src(paths.teaStyles)
+function teaStyles() {
+	return gulp.src(paths.teaStyles.src)
 		.pipe(sourcemaps.init())
 			.pipe(postcss(processors))
 		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest('tea/build/'));
-});
+		.pipe(gulp.dest(paths.teaStyles.dest));
+}
 
-gulp.task(function lintStyles() {
-	return gulp.src(paths.styles)
+function lintStyles() {
+	return gulp.src(paths.styles.src)
 		.pipe(postcss(processors))
-		.pipe(gulp.dest('styles/build/'));
-});
+		.pipe(gulp.dest(paths.styles.dest));
+}
 
-gulp.task(function lint() {
-	return gulp.src(paths.builtStyles)
+function lint() {
+	return gulp.src(paths.builtStyles.src)
 		.pipe(lint({
 			failAfterError: false,
 			reporters: {
@@ -72,9 +92,9 @@ gulp.task(function lint() {
 			},
 			debug: true
 	    }));
-});
+}
 
-gulp.task(function sprites() {
+function sprites() {
 	var options = {
 		shape: {
 			transform       : [
@@ -94,74 +114,83 @@ gulp.task(function sprites() {
 		} 
 	};
     
-	return gulp.src(paths.sprites)
+	return gulp.src(paths.sprites.src)
 		.pipe(sprite(options))
-		.pipe(gulp.dest('images/Social Icons'));
-});
+		.pipe(gulp.dest(paths.sprites.dest));
+}
 
-gulp.task(function mainScript() {
+function mainScript() {
 	return gulp.src(paths.mainScript)
 		.pipe(sourcemaps.init())
 			.pipe(concat('main.js'))
 			.pipe(uglify())
 		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest('scripts/build/'));
-});
+		.pipe(gulp.dest(paths.scripts.dest));
+}
 
-gulp.task(function homeScript() {
+function homeScript() {
 	return gulp.src(paths.homeScript)
 		.pipe(sourcemaps.init())
 			.pipe(concat('home.js'))
 			.pipe(uglify())
 		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest('scripts/build/'));
-});
+		.pipe(gulp.dest(paths.scripts.dest));
+}
 
-gulp.task(function socialIconsScript() {
+function socialIconsScript() {
 	return gulp.src(paths.socialIconsScript)
 		.pipe(sourcemaps.init())
 			.pipe(concat('social-icons.js'))
 			.pipe(uglify())
 		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest('scripts/build/'));
-});
+}
 
-gulp.task('scripts', gulp.parallel('mainScript', 'homeScript', 'socialIconsScript', function(done) {
-	return gulp.src(paths.scripts)
+var scripts = gulp.parallel(mainScript, homeScript, socialIconsScript, function normalScripts(done) {
+	return gulp.src(paths.scripts.src)
 		.pipe(sourcemaps.init())
 			.pipe(uglify())
 		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest('scripts/build/'));
+		.pipe(gulp.dest(paths.scripts.dest));
 	done();
-}));
+})
 
-gulp.task(function sitemap() {
-	return gulp.src(paths.sitemap)
+function sitemap() {
+	return gulp.src(paths.sitemap.src)
 		.pipe(sitemap({
 			siteUrl: 'https://scotthsmith.com'
 		}))
-		.pipe(gulp.dest('./'));
-});
+		.pipe(gulp.dest(paths.sitemap.dest));
+}
 
-gulp.task(function watch() {
-	gulp.watch(paths.html, gulp.series('html'));
-	gulp.watch(paths.styles, gulp.series('styles'));
-	gulp.watch(paths.sprites, gulp.series('sprites'));
+function watch() {
+	gulp.watch(paths.html.src, gulp.series('html'));
+	gulp.watch(paths.styles.src, gulp.series('styles'));
+	gulp.watch(paths.sprites.src, gulp.series('sprites'));
 	gulp.watch('scripts/*.js', gulp.series('scripts'));
-});
+}
 
 // Workflows
 // $ gulp: Builds, prefixes, and minifies CSS files; concencates and minifies JS files; watches for changes. The works.
-gulp.task('default', gulp.parallel('html', 'styles', 'teaStyles', 'sprites', 'scripts', 'watch', function(done) {
-	done();
-}));
+var defaultTask = gulp.parallel(html, styles, teaStyles, sprites, scripts, watch);
 
 // $ gulp build: Builds, prefixes, and minifies CSS files; concencates and minifies JS files. For deployments.
-gulp.task('build', gulp.parallel('html', 'styles', 'teaStyles', 'sprites', 'scripts', function(done) {
-	done();
-}));
+var buildTask = gulp.parallel(html, styles, teaStyles, sprites, scripts);
 
 // $ gulp test: Runs stylelint against built CSS files. For CI.
-gulp.task('pretest', gulp.parallel('lintStyles', function(done) {
-	done();
-}));
+var pretestTask = gulp.parallel(lintStyles);
+
+// Exports
+// Externalise individual tasks.
+exports.html = html;
+exports.styles = styles;
+exports.lint = lint;
+exports.sprites = sprites;
+exports.scripts = scripts;
+exports.sitemap = sitemap;
+exports.watch = watch;
+
+// Externalise Workflows.
+exports.build = buildTask;
+exports.prestest = pretestTask;
+exports.default = defaultTask;
