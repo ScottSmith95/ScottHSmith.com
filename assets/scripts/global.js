e504adaae854e2b4aef850d56902fb335dc288ac
Scott Smith <mail@ScottHSmith.com>
2019-10-25 15:35:50 -0700

    Add semicolon.

--- assets/scripts/global.js
+++ assets/scripts/global.js
@@ -1,6 +1,6 @@
 'use strict';
 
-document.documentElement.className = document.documentElement.className.replace( 'no-js', 'js' )
+document.documentElement.className = document.documentElement.className.replace( 'no-js', 'js' );
 
 if ( 'serviceWorker' in navigator ) {
 // 	navigator.serviceWorker.register( '/service-worker.js' );
