0f8c6eef59d0c8ed91e08a1fc2fceb2e00edffcb
Scott Smith <mail@ScottHSmith.com>
2026-02-02 13:12:43 -0800

    Inline the script that updates class for JS support.

--- assets/scripts/global.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-document.documentElement.className = document.documentElement.className.replace( 'no-js', 'js' );
-
-if ( 'serviceWorker' in navigator ) {
-// 	navigator.serviceWorker.register( '/service-worker.js' );
-}
